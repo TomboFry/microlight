c1841f1363e0656d592ea5b0bde6b76a20370ed7
Tom Gardiner <tom@railpoint.co.uk>
2018-07-22 21:08:19 +0100

    Add non-functioning install page
    This page will be displayed if the database has not already been set up
    properly. It will prompt a new user to set up the database with an
    identity (so they can log in using IndieAuth and start creating posts).

---
+++ install.php
@@ -0,0 +1,190 @@
+<?php
+
+define('MICROLIGHT_INIT', true);
+require_once('includes/config.php');
+
+if (isset($_POST['submit'])) {
+	echo "<pre>" . var_export($_POST, true) . "</pre>";
+} else {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Install Microlight</title>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		body {
+			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+			width: 100%;
+			padding: 64px;
+			max-width: 640px;
+			margin: 0 auto;
+			color: #333;
+			line-height: 1;
+		}
+		body > * {
+			margin-bottom: 16px;
+		}
+		h1 {
+			font-weight: 300;
+			font-size: 32pt;
+		}
+		.p {
+			line-height: 1.4em;
+		}
+		.f {
+			display: block;
+			width: 100%;
+			padding: 24px 0;
+		}
+		.f label {
+			display: block;
+			font-weight: 500;
+			margin-bottom: 8px;
+		}
+		.f input[type='text'], .f input[type='email'], .f input[type='url'] {
+			display: block;
+			width: 100%;
+			padding: 8px;
+			font-size: 24px;
+			font-weight: 300;
+			border: 1px solid #ccc;
+			color: #555;
+		}
+		#l input[type='text'], #l input[type='email'], #l input[type='url'] {
+			display: inline-block;
+			width: 50%;
+			margin-bottom: 8px;
+			font-size: 18px;
+		}
+		.i-left {
+			border-right: 0 !important;
+		}
+		.d {
+			display: block;
+			margin-top: 8px;
+			font-size: 9pt;
+			color: #999;
+		}
+		.b {
+			margin-top: 0;
+			padding-bottom: 0;
+		}
+		.r {
+			color: #E34;
+			font-weight: 700;
+			margin-left: 8px;
+			font-size: 10pt;
+			float: right;
+		}
+		.w {
+			padding: 16px;
+			border: 1px solid #A12;
+			background-color: #FDE;
+			color: #E34;
+			border-radius: 6px;
+		}
+		#install {
+			background: #4A3;
+			background: linear-gradient(#5B5, #4A3);
+			border: 0;
+			border-bottom: 3px solid #184;
+			padding: 16px;
+			width: 100%;
+			color: #F2F2F2;
+			font-weight: 700;
+			font-size: 16px;
+			border-radius: 6px;
+			transition: margin-top 0.1s, border 0.1s;
+		}
+		#install:active {
+			margin-top: 2.5px;
+			border-bottom: 0.5px solid #184;
+		}
+	</style>
+</head>
+<body>
+	<h1>Install Microlight</h1>
+	<p class='p w'>
+		You are viewing this page because Microlight has not been
+		completely set up. You will need to create an identity to begin
+		using Microlight.
+	</p>
+	<form action='' method='POST'>
+		<div class='f'>
+			<label for='name'>
+				Full Name
+				<span class='r'>required</span>
+			</label>
+			<input required type='text' name='name' id='name' />
+		</div>
+		<div class='f'>
+			<label for='email'>
+				Email Address
+				<span class='r'>required</span>
+			</label>
+			<input required type='email' name='email' id='email' />
+			<span class='d p'>
+				Your email is not sent to me, it is simply to
+				display on your homepage as contact information.
+				Also, if no social media profiles are provided
+				below, you will still be able to log into this
+				blog to administer it.
+			</span>
+		</div>
+		<div class='f'>
+			<label for='note'>Note / Tagline</label>
+			<input type='text' name='note' id='note' />
+		</div>
+		<div class='f'>
+			<label for='l'>
+				Social Media Accounts
+			</label>
+			<div id='l'>
+				<div class='account'>
+					<input
+						type='text'
+						placeholder='Name (eg. "Twitter")'
+						name='sm_service[]'
+						class='i-left'
+					/><input type='url' placeholder='URL' name='sm_url[]' />
+				</div>
+				<div class='account'>
+					<input
+						type='text'
+						placeholder='Name (eg. "GitHub")'
+						name='sm_service[]'
+						class='i-left'
+					/><input type='url' placeholder='URL' name='sm_url[]' />
+				</div>
+				<div class='account'>
+					<input
+						type='text'
+						placeholder='Name'
+						name='sm_service[]'
+						class='i-left'
+					/><input type='url' placeholder='URL' name='sm_url[]' />
+				</div>
+			</div>
+			<span class='d p b'>
+				To authenticate using your website with one of
+				these social media accounts (instead of email),
+				<a href='<?php echo ml_base_url(); ?>'>your
+				homepage</a> must appear on the accounts you
+				have specified.
+			</span>
+		</div>
+		<div class='f b'>
+			<input id='install' name='submit' type='submit' value='Install' />
+		</div>
+	</form>
+</body>
+</html>
+<?php } die(); ?>
