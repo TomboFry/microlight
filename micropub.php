548db3e8f25b19e1aac40875a25bd4ba76f98768
Tom Gardiner <tom@tombofry.co.uk>
2018-07-31 23:00:04 +0100

    Create slug and summary fields for MicroPub

--- micropub.php
+++ micropub.php
@@ -32,8 +32,8 @@ if ($content_type === 'application/json') {
 
 function post($key) {
 	global $post;
-	if (isset($post[$key]) && !empty($post[$key])) return $post[$key];
 
+	if (isset($post[$key]) && !empty($post[$key])) return $post[$key];
 	return false;
 }
 
@@ -52,17 +52,41 @@ function showError($error = ResponseCode::SERVER_ERROR, $description = '') {
 	return;
 }
 
+// https://stackoverflow.com/a/2955878
+function slugify($text) {
+	// replace non letter or digits by -
+	$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+	// transliterate
+	$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+	// remove unwanted characters
+	$text = preg_replace('~[^-\w]+~', '', $text);
+
+	// trim
+	$text = trim($text, '-');
+
+	// remove duplicate -
+	$text = preg_replace('~-+~', '-', $text);
+
+	// lowercase
+	$text = strtolower($text);
+
+	return $text;
+}
+
 function processRequest () {
 	$h = post('h');
 	$name = post('name');
 	$content = post('content');
+	$summary = post('summary');
 	$photo = post('photo');
 	$url = post('url');
 	$categories = post('category');
 
 	// Variables not necessarily set by the POST data
 	$type = 'article';
-	$slug = $name;
+	$slug = '';
 	
 	// h parameter is required
 	if ($h === false) return showError(ErrorCodes::INVALID_REQUEST, 'Field \'h\' required');
@@ -70,15 +94,30 @@ function processRequest () {
 	// If a name is not provided, assume it's a note (for now)
 	if ($name === false) $type = 'note';
 
-	if ($slug === '' || $slug === false) {
-		$slug = date('omd-His');
+	// Create a summary from the content, if one was not provided
+	if ($summary === '' || $summary === false) {
+		// Limit to 160 characters, add ellipsis if it's longer
+		$summary = preg_replace('/^\#(.*)\R+/', '', $content);
+		$summary = preg_split('/$\R?^/m', $summary)[0];
+		$summary = substr($summary, 0, 157);
+		if (strlen($summary) === 157) $summary .= '...';
 	}
+	
+	if ($slug === '' || $slug === false) {
+		$slug = slugify(implode('-', array_slice(preg_split('/\s/m', $summary), 0, 5)));
+		$slug = date('omd') . '-' . $slug;
+	} else {
+		$slug = date('omd') . '-' . slugify($slug);
+	}
+
+	$categories = implode(',', $categories) . ',';
 
 	header('Content-Type: application/json');
 	echo json_encode([
 		'files' => $_FILES,
 		'h' => $h,
 		'name' => $name,
+		'summary' => $summary,
 		'content' => $content,
 		'photo' => $photo,
 		'url' => $url,
